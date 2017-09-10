8a792951d8a7a015474f4c8f5e665f366c800e54
root <root@localhost.localdomain>
2017-09-10 07:07:45 +0000

    ItemsControllerの雛形を作成

---
+++ app/controllers/ItemsController.php
@@ -0,0 +1,37 @@
+<?php
+
+class ItemsController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+    
+    public function searchAction()
+    {
+        
+    }
+    
+    public function searchResultAction()
+    {
+        
+    }
+    
+    public function newAction()
+    {
+        
+    }
+    
+    public function updateAction()
+    {
+        
+    }
+    
+    public function destroyAction()
+    {
+        
+    }
+
+}
+
