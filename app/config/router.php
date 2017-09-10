9cf6b259d73701377aa1a4b582d4692ac64c37a1
root <root@localhost.localdomain>
2017-09-10 07:06:18 +0000

    ルートを指示

--- app/config/router.php
+++ app/config/router.php
@@ -2,6 +2,78 @@
 
 $router = $di->getRouter();
 
-// Define your routes here
+/**
+ * 商品情報取得
+ * http://localhost/items/
+ * GETでアクセス
+ * ItemsControllerのindexメソッドが実行
+ */
+$router->addGet(
+    '/items/',
+    'Items::index'
+);
+
+/**
+ * 登録
+ * http://localhost/items/
+ * POSTでアクセス
+ * ItemsControllerのnewメソッドが実行
+ */
+$router->addPost(
+    '/items/',
+    'Items::new'
+    );
+
+/**
+ * 検索画面
+ * http://localhost/items/search/
+ * GETでアクセス
+ * ItemsControllerのsearchメソッドが実行
+ */
+$router->addGet(
+    '/items/search',
+    'Items::search'
+    );
+
+/**
+ * 検索結果
+ * http://localhost/items/search/{name}
+ * GETでアクセス（）
+ * ItemsControllerのsearchResultメソッドが実行
+ */
+$router->addGet(
+    '/items/search/{name}',
+    'Items::searchResult'
+);
+
+/**
+ * 更新
+ * http://localhost/items/{id:[0-9]+}/
+ * PUTでアクセス
+ * ItemsControllerのupdateメソッドが実行
+ */
+$router->add(
+    '/items/{id:[0-9]+}',
+    'Items::update',
+    )->via(
+        [
+            'PUT'
+        ]
+);
+
+/**
+ * 削除
+ * http://localhost/items/{id:[0-9]+}
+ * DELETEでアクセス
+ * ItemsControllerのdestroyメソッドが実行
+ */
+$router->add(
+    '/items/{id:[0-9]+}',
+    'Items::destroy',
+    )->via(
+        [
+            'DELETE'
+        ]
+);
 
 $router->handle();
