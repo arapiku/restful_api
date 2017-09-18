e7a9fb81c400759b44df7f3ae5baf1c92630ad37
root <root@localhost.localdomain>
2017-09-18 04:55:49 +0000

    sqlのテストファイルをアップロード

---
+++ items.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
+--
+-- Host: localhost    Database: rest
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'aaa','aaa',3,'aaa'),(2,'title2','desc2',20000,'bbb'),(3,'title3','desc3',30000,'ccc'),(4,'title4','desc4',222222222,'ddd'),(5,'title5','desc5',1321,'eee'),(10,'title','desc',123,'aaaa'),(11,'title','desc',123,'aaaa'),(12,'title','desc',123,'aaaa'),(13,'titletitlet','desc',123,'aaaa'),(14,'titletitletfjeaofjaoejfaeo','desc',123,'aaaa'),(15,'titletitlet','desc',123,'aaaa'),(16,'ああ','ddd',12,'aaa'),(17,'ああ','いいい',12,'aaa'),(18,'titletitletひらがな','desc漢字',123,'aaaa'),(19,'titletitletひらがな','desc漢字',123,'aaaa'),(20,'titletitletひらがな','desc漢字',123,'@/vagrant/test1.jpg'),(21,'titletitletひらがな','desc漢字',123,'@/vagrant/test1.jpg'),(22,'titletitletひらがな','desc漢字',123,'@/vagrant/test1.jpg'),(23,'titletitletひらがな','desc漢字',123,'@/vagrant/test1.jpg'),(24,'あいうえおかきくけこさしすせそ','desc漢字',123,'@/vagrant/test1.jpg'),(25,'あいうえおかきくけこさしすせそ','desc漢字',123,'@/vagrant/test1.jpg'),(26,'あいうえおかきくけこさしすせそ','desc漢字',123,'@/vagrant/test1.jpg'),(27,'あいうえおかきくけこさしすせそ','desc漢字',123,'@/vagrant/test1.jpg'),(28,'あいうえおかきくけこさしすせそ','desc',123,'@/vagrant/test1.jpg'),(29,'あいうえおかきくけこさしすせそ','desc漢字',123,'@/vagrant/test1.jpg'),(30,'漢字','ひらがな',123,'@/vagrant/test1.jpg');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-18  4:55:08
