57b7deb9aa5fe39c7774273db66946181e72cdd0
root <root@localhost.localdomain>
2017-10-04 14:56:51 +0000

    まだ機能してない

--- app/library/logger.php
+++ app/library/logger.php
@@ -2,18 +2,28 @@
 
 use Phalcon\Logger;
 use Phalcon\Logger\Adapter\Stream as StreamAdapter;
+use Phalcon\Logger\Adapter\File as FileAdapter;
 
 class Logger
 {
-    private static final $plogger = new StreamAdapter("php://stderr");
+    private static final $plogger = new StreamAdapter("php://stdout");
     
+//     $plogger = new FileAdapter(
+//         'app/logs/error.log',
+//         [
+//             'mode' => 'w',
+//         ]
+//     );
     
     public static function error($args) {
-        $plogger->log(Phalcon\Logger::ERROR, $args);
+        $plogger->error($args);
     }
     
     public static function info($args) {
-        $plogger->log(Phalcon\Logger::ERROR, $args);
+        $plogger->info($args);
     }
     
+    public static function debug($args) {
+        $plogger->debug($args);
+    }
 }
