2d46faf32810f1f4dd40f06969b33361072b0297
root <root@localhost.localdomain>
2017-10-09 06:57:53 +0000

    各actionにログを仕込む

--- app/controllers/ItemsController.php
+++ app/controllers/ItemsController.php
@@ -8,225 +8,266 @@ use Phalcon\Mvc\Model\Query;
 use Phalcon\Http\Response;
 
 use Phalcon\Logger;
-// use Phalcon\Logger\Adapter\Stream as StreamAdapter;
-// use Phalcon\Logger\Adapter\File as FileAdapter;
 
 class ItemsController extends \Phalcon\Mvc\Controller
 {
 
     public function indexAction()
     {
-        // 全件検索、json化
+        // 全件検索
         $items = Items::find();
-        $json = json_encode($items);
         
-        // ログ出力
-        $plogger = new Plogger("データ取得ok");
-        $plogger->debug();
-        var_dump($plogger);
-        
-        // レスポンス
-        $this->response->setContent($json);
-        $this->response->send();
-        $this->view->disable();
+        // 検索が成功すれば
+        if($items) {
+            //json化
+            $json = json_encode($items);
+            // ログ出力
+            $plogger = new Plogger("データ取得成功");
+            $plogger->debug();
+            echo "<pre>";
+            var_dump($plogger);
+            echo "</pre>";
+            // レスポンス
+            $this->response->setContent($json);
+            $this->response->send();
+            $this->view->disable();
+            
+        // 検索が失敗すれば
+        } else {
+            // ログ出力
+            $plogger = new Plogger("データ取得失敗");
+            $plogger->debug();
+            echo "<pre>";
+            var_dump($plogger);
+            echo "</pre>";
+            // レスポンス
+            $this->response->setStatusCode(404, 'NOT-FOUND');
+            $this->view->disable();
+        }
         
         return;
     }
     
     public function searchAction($title)
     {
+        // 商品タイトル検索
         $items = Items::findByTitle($title);
-        echo json_encode($items);
+        
+        // 検索結果が1件以上であれば
+        if(count($items) != 0) {
+            // json化
+            $json = json_encode($items);
+            // ログ出力
+            $plogger = new Plogger("検索結果が".count($items)."件見つかりました");
+            $plogger->debug();
+            echo "<pre>";
+            var_dump($plogger);
+            echo "</pre>";
+            // レスポンス
+            $this->response->setContent($json);
+            $this->response->send();
+            $this->view->disable();
+        // 検索結果が0件であれば
+        } else {
+            // ステータスコードを変える
+            $this->response->setStatusCode(404, 'NOT-FOUND');
+            // ログ出力
+            $plogger = new Plogger("検索結果が見つかりませんでした");
+            $plogger->debug();
+            echo "<pre>";
+            var_dump($plogger);
+            echo "</pre>";
+            // レスポンス
+            $this->view->disable();
+        }
+            
+        return;
     }
     
     public function singleAction($id)
-    {               
+    {
+        // id検索
         $items = Items::findById($id);
         
-        // レスポンスを作成
-        $response = new Response();
+        // 検索結果が見つかれば
+        if (count($items) != 0) {
+            // json化
+            $json = json_encode($items);
+            // ログ出力
+            $plogger = new Plogger("検索結果が見つかりました");
+            $plogger->debug();
+            echo "<pre>";
+            var_dump($plogger);
+            echo "</pre>";
+            // レスポンス
+            $this->response->setContent($json);
+            $this->response->send();
+            $this->view->disable();
         
-        if (count($items) <= 0) {
-            // ステータスコードを変える
-            $response->setStatusCode(404, 'NOT-FOUND');
-            
-            $response->setJsonContent(
-                [
-                    'status' => 'NOT-FOUND'
-                ]
-            );
-            
+        // 検索結果が見つからなければ
         } else {
-            $response->setJsonContent(
-                [
-                    'status' => 'FOUND',
-                    'data' => $items
-                ]
-            );
+            // ステータスコードを変える
+            $this->response->setStatusCode(404, 'NOT-FOUND');
+            // ログ出力
+            $plogger = new Plogger("検索結果が見つかりませんでした");
+            $plogger->debug();
+            echo "<pre>";
+            var_dump($plogger);
+            echo "</pre>";
+            // レスポンス
+            $this->view->disable();
         }
         
-        return $response;
+        return;
     }
     
     public function newAction()
     {
         $items = $this->request->getJsonRawBody();
-        
-
+        echo "作成データ：";
+        var_dump($items);
         $status = Items::createItems($items);
-        
-        // レスポンスを作成
-        $response = new Response();
         
         // もしinsertionが成功したら
         if ($status->success() == true) {
-            // HTTPステータスコードを変える
-            $response->setStatusCode(201, 'Created');
-            
+            // json化
+            $json = json_encode($status);
+            // ログ出力
+            $plogger = new Plogger("データの作成に成功しました");
+            $plogger->debug();
+            var_dump($plogger);
+            // レスポンス
+            $this->response->setStatusCode(201, 'Created');
             $items->id = $status->getModel()->id;
+            $this->response->setContent($json);
+            $this->response->send();
+            $this->view->disable();
             
-            $response->setJsonContent(
-                [
-                    'status' => 'OK',
-                    'data' => $items,
-                ]
-            );
-            // 失敗したら
+        // insertionが失敗したら
         } else {
-            // HTTPステータスコードを変える
-            $response->setStatusCode(409, 'Conflict');
-            
+            // ログ出力
+            $plogger = new Plogger("データの作成に失敗しました");
+            $plogger->debug();
+            var_dump($plogger);
+            // エラーメッセージの用意
             $errors = [];
-            
             foreach($status->getMessages() as $message) {
                 $errors[] = $message->getMessage();
             }
-            
-            $response->setJsonContent(
-                [
-                    'status' => 'ERROR',
-                    'messages' => $errors,
-                    'data1' => $items,
-                    'data2' => $status,
-                ]
-            );
-            
+            // レスポンス
+            $this->response->setStatusCode(409, 'Conflict');
+            $this->response->setContent(var_dump($errors));
+            $this->response->send();
+            $this->view->disable();
         }
         
-        return $response;
+        return;
     }
     
     public function updateAction($id)
     {
+        // id検索
         $find_items = Items::findById($id);
         
-        // レスポンスを作成
-        $response = new Response();
-        
-        // 対象データが存在しなかったら
-        if (count($find_items) <= 0) {
-            // ステータスコードを変える
-            $response->setStatusCode(404, 'NOT-FOUND');
-            
-            $response->setJsonContent(
-                [
-                    'status' => 'NOT-FOUND'
-                ]
-            );
-            
         // 対象データが存在したら
-        } else {
+        if (count($find_items) != 0) {
             
             $items = $this->request->getJsonRawBody();
-
+            echo "更新データ：";
+            var_dump($items);
             $status = Items::updateItems($items, $id);
             
             // もしアップデートが成功したら
             if ($status->success() === true) {
-                // HTTPステータスコードを変える
-                $response->setStatusCode(204, 'No Content');
+                // json化
+                $json = json_encode($status);
+                // ログ出力
+                $plogger = new Plogger("データの更新に成功しました");
+                $plogger->debug();
+                var_dump($plogger);
+                // レスポンス
+                $this->response->setStatusCode(201, 'Updated');
+                $items->id = $status->getModel()->id;
+                $this->response->setContent($json);
+                $this->response->send();
+                $this->view->disable();
                 
-                $response->setJsonContent(
-                    [
-                        'status' => 'OK',
-                        'message' => $find_items
-                    ]
-                );
             // 失敗したら
             } else {
-                // HTTPステータスコードを変える
-                $response->setStatusCode(409, 'Conflict');
-                
+                // ログ出力
+                $plogger = new Plogger("データの更新に失敗しました");
+                $plogger->debug();
+                var_dump($plogger);
+                // エラーメッセージの用意
                 $errors = [];
-                
                 foreach($status->getMessages() as $message) {
                     $errors[] = $message->getMessage();
                 }
-                
-                $response->setJsonContent(
-                    [
-                        'status' => 'ERROR',
-                        'messages' => $errors,
-                    ]
-                );
+                // レスポンス
+                $this->response->setStatusCode(409, 'Conflict');
+                $this->response->setContent(var_dump($errors));
+                $this->response->send();
+                $this->view->disable();
             }
             
+        // 対象データが存在しなかったら
+        } else {
+            // ログ出力
+            $plogger = new Plogger("対象のデータは存在しませんでした");
+            $plogger->debug();
+            var_dump($plogger);
+            // レスポンス
+            $this->response->setStatusCode(404, 'NOT-FOUND');
+            $this->view->disable();
         }
         
-        return $response;
+        return;
     }
     
     public function destroyAction($id)
     {
+        // id検索
         $find_items = Items::findById($id);
         
-        // レスポンスを作成
-        $response = new Response();
-        
-        // 対象データが存在しなかったら
-        if (count($find_items) <= 0) {
-            $response->setStatusCode(404, 'Not Found');
-            
-            $response->setJsonContent(
-                [
-                    'status' => 'NOT-FOUND'
-                ]
-            );
         // 対象データが存在したら
-        } else {
+        if (count($find_items) != 0) {
+            
+            // 対象データを削除
             $status = Items::deleteItems($id);
             
             // もし削除が成功したら
             if ($status->success() === true) {
-                // HTTPステータスコードを変える
-                $response->setStatusCode(204, 'No Content');
-                
-                $response->setJsonContent(
-                    [
-                        'status' => 'OK'
-                    ]
-                );
-            // 失敗したら
+                // レスポンス
+                $this->response->setStatusCode(204, 'No Content');
             } else {
-                // HTTPステータスコードを変える
-                $response->setStatusCode(409, 'Conflict');
-                
+                // ログ出力
+                $plogger = new Plogger("データの削除に失敗しました");
+                $plogger->debug();
+                var_dump($plogger);
+                // エラーメッセージの用意
                 $errors = [];
-                
                 foreach($status->getMessages() as $message) {
                     $errors[] = $message->getMessage();
                 }
-                
-                $response->setJsonContent(
-                    [
-                        'status' => 'ERROR',
-                        'messages' => $errors,
-                    ]
-                );
+                // レスポンス
+                $this->response->setStatusCode(409, 'Conflict');
+                $this->response->setContent(var_dump($errors));
+                $this->response->send();
+                $this->view->disable();
             }
+            
+        // 対象データが存在しなかったら
+        } else {
+            // ログ出力
+            $plogger = new Plogger("対象のデータは存在しませんでした");
+            $plogger->debug();
+            var_dump($plogger);
+            // レスポンス
+            $this->response->setStatusCode(404, 'NOT-FOUND');
+            $this->view->disable();
         }
         
-        return $response;
+        return;
     }
 
 }
